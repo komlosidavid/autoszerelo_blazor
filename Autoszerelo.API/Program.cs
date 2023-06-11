6f047b4b4cc272d57f935631c90613f78cfeae75
Dávid Komlósi <komlosidavid19990410@gmail.com>
2023-06-11 21:43:29 +0200

    Added initial database context and dependeny injection configs.

--- Autoszerelo.API/Program.cs
+++ Autoszerelo.API/Program.cs
@@ -1,3 +1,8 @@
+using Autoszerelo.API.Data;
+using Autoszerelo.API.Repoitories;
+using Autoszerelo.API.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CarRepairSystem")));
+
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
 
 var app = builder.Build();
 
