39cf6ef7e6e3cd2fbb48114851edb1b277581638
Dávid Komlósi <komlosidavid19990410@gmail.com>
2023-06-13 19:38:37 +0200

    Changed neccessary code

--- Autoszerelo.Client.Office/Program.cs
+++ Autoszerelo.Client.Office/Program.cs
@@ -1,4 +1,5 @@
 using Autoszerelo.Client.Office;
+using Autoszerelo.Client.Office.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<WorkService>();
+builder.Services.AddScoped<ClientService>();
+builder.Services.AddScoped<CarService>();
 
 await builder.Build().RunAsync();
